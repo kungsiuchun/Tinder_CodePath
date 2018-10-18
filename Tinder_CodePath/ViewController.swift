df6ffab4518a98fa25dc15953de679900d33b74c
kungsiuchun <skung@mail.sfsu.edu>
2018-10-17 17:16:05 -0700

    Initial Commit

---
+++ Tinder_CodePath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder_CodePath
+//
+//  Created by SiuChun Kung on 10/17/18.
+//  Copyright © 2018 SiuChun Kung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
